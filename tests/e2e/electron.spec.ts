2bef50dcc2cb81808181eadf9b0faa5c15e9ee1c
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-04-27 14:53:18 +0200

    all packages ready for code transfer

--- tests/e2e/electron.spec.ts
+++ tests/e2e/electron.spec.ts
@@ -1,5 +1,8 @@
-import { testWithSpectron } from "vue-cli-plugin-electron-builder";
-import spectron from "spectron"
+/**
+ * @jest-environment node
+ */
+const spectron = require("spectron");
+const { testWithSpectron } = require("vue-cli-plugin-electron-builder");
 jest.setTimeout(2 * 60 * 1000);
 
 test("Window Loads Properly", async () => {
@@ -11,13 +14,13 @@ test("Window Loads Properly", async () => {
   // Window was created
   expect(await client.getWindowCount()).toBe(1);
   // It is not minimized
-  // expect(await win.isMinimized()).toBe(false);
-  // // Window is visible
-  // expect(await win.isVisible()).toBe(true);
-  // // Size is correct
-  // const { width, height } = await win.getBounds();
-  // expect(width).toBeGreaterThan(0);
-  // expect(height).toBeGreaterThan(0);
+  expect(await win.isMinimized()).toBe(false);
+  // Window is visible
+  expect(await win.isVisible()).toBe(true);
+  // Size is correct
+  const { width, height } = await win.getBounds();
+  expect(width).toBeGreaterThan(0);
+  expect(height).toBeGreaterThan(0);
   // App is loaded properly
   // expect(
   //   /Welcome to Your Vue\.js (\+ TypeScript )?App/.test(
