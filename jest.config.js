e5aba5c5a2be7a14995a058ce89d4f8d080238f2
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-04-27 12:45:38 +0200

    jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: [
+    "**/tests/unit/**/*.spec.[jt]s?(x)",
+    "**/tests/e2e/**/*.spec.[jt]s?(x)",
+    "**/__tests__/*.[jt]s?(x)"
+  ],
+  transformIgnorePatterns: ["/node_modules/", "/dist_electron/"]
 };
