77bc10bee92c19a32540e33559d8213dfaf7340f
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-05-08 16:22:04 +0200

    implemented BaseIcon

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,10 +1,78 @@
 <template>
-  <div></div>
+  <img
+    v-if="showIcon('close')"
+    svg-inline
+    src="@/assets/icons/close.svg"
+    :alt="iconAlt"
+    :class="classes"
+  />
+  <img
+    v-else-if="showIcon('pause')"
+    svg-inline
+    src="@/assets/icons/pause.svg"
+    :alt="iconAlt"
+    :class="classes"
+  />
+  <img
+    v-else-if="showIcon('settings')"
+    svg-inline
+    src="@/assets/icons/settings.svg"
+    :alt="iconAlt"
+    :class="classes"
+  />
+  <img
+    v-else-if="showIcon('skip')"
+    svg-inline
+    src="@/assets/icons/skip.svg"
+    :alt="iconAlt"
+    :class="classes"
+  />
+  <img
+    v-else-if="showIcon('start')"
+    svg-inline
+    src="@/assets/icons/start.svg"
+    :alt="iconAlt"
+    :class="classes"
+  />
+  <img
+    v-else-if="showIcon('stop')"
+    svg-inline
+    src="@/assets/icons/stop.svg"
+    :alt="iconAlt"
+    :class="classes"
+  />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 // import mixins from 'vue-typed-mixins'
 
-export default Vue.extend({})
+export default Vue.extend({
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    showIcon(name: string): boolean {
+      return this.icon === name
+    },
+  },
+  computed: {
+    iconAlt(): string {
+      return `${this.icon} icon`
+    },
+    classes() {
+      return [
+        'fill-current',
+        this.dark ? 'text-primary-800' : 'text-primary-100',
+      ]
+    },
+  },
+})
 </script>
--- tests/unit/components/BaseIcon.spec.ts
+++ tests/unit/components/BaseIcon.spec.ts
@@ -2,7 +2,29 @@ import { render } from '@testing-library/vue'
 import Component from "@/components/BaseIcon.vue";
 
 describe("components/BaseIcon.vue", () => {
-  test("", () => {
-    const { getByText } = render(Component)
+  test("set icon based on prop", async () => {
+    const { getByAltText, updateProps } = render(Component, {
+      props:{
+        icon: 'close'
+      }
+    })
+    const icons = ['close', 'pause', 'settings', 'skip', 'start', 'stop']
+    for (const icon of icons) {
+      await updateProps({icon})
+      expect(getByAltText(`${icon} icon`)).toBeVisible()
+    }
+
+  });
+  test("set icon colour by dark prop", async () => {
+    const { getByAltText, updateProps } = render(Component, {
+      props:{
+        icon: 'close',
+      }
+    })
+
+    expect(getByAltText('close icon')).toHaveClass("fill-current","text-primary-100")
+
+    await updateProps({icon: 'close', dark: true})
+    expect(getByAltText('close icon')).toHaveClass("fill-current","text-primary-800")
   });
 });
