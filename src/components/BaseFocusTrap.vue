8c9dab3bedcda6199753ec86a349ea9586de7699
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-05-13 12:00:44 +0200

    implemented focus trap

--- src/components/BaseFocusTrap.vue
+++ src/components/BaseFocusTrap.vue
@@ -1,13 +1,55 @@
-<template>
-  <div></div>
-</template>
-
 <script lang="ts">
-import Vue from 'vue'
+import Vue, { VNode } from 'vue'
 // import mixins from 'vue-typed-mixins'
-
+import createFocusTrap, { FocusTrap } from 'focus-trap'
 
 export default Vue.extend({
+  render(): VNode {
+    const slot = this.$slots.default
+    if (!slot || !slot.length || slot.length > 1)
+      throw new Error('BaseFocusTrap requires exactly one child')
 
+    return slot[0]
+  },
+  props: {
+    active: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      focusTrap: null,
+    } as {
+      focusTrap: FocusTrap | null
+    }
+  },
+  watch: {
+    active: {
+      handler(activate): void {
+        if (activate) {
+          const component = this.$el as HTMLElement
+          const trap = createFocusTrap(component)
+          trap.activate()
+          this.focusTrap = trap
+        } else {
+          this.deactivateTrap()
+        }
+      },
+      immediate: true,
+    },
+  },
+  beforeDestroy() {
+    this.deactivateTrap()
+  },
+  methods: {
+    deactivateTrap(): void {
+      const focusTrap = this.focusTrap
+      if (focusTrap) {
+        focusTrap.deactivate()
+        this.focusTrap = null
+      }
+    },
+  },
 })
 </script>
