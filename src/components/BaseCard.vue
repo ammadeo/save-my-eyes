239b68d7d832745adf941461456b749f3472bf2d
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-05-13 17:59:33 +0200

    improved colors

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -4,10 +4,10 @@
     :class="cardClasses"
   >
     <div
-      class="rounded-lg absolute bg-secondary-1050 shadow-inner-1 inset-0 top-0 left-0 max-h-full"
+      class="rounded-lg absolute bg-secondary-1075 shadow-inner-1 inset-0 top-0 left-0 max-h-full"
     ></div>
     <div
-      class="flex flex-1 flex-col max-h-full  rounded-lg overflow-auto bg-gradient-card scroll-smooth scroll-sm relative transform  -translate-y-1"
+      class="flex flex-1 flex-col max-h-full  rounded-lg overflow-auto bg-gradient-card scroll-smooth scroll-sm relative transform  -translate-y-2"
     >
       <slot />
     </div>
