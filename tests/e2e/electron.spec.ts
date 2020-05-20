23d5392d9d8c2a83a772579d7312f633ce30daf2
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-05-20 12:27:11 +0200

    made app start with error

--- tests/e2e/electron.spec.ts
+++ tests/e2e/electron.spec.ts
@@ -1,32 +1,51 @@
 /**
  * @jest-environment node
  */
-import spectron from 'spectron'
-import { testWithSpectron } from 'vue-cli-plugin-electron-builder'
-jest.setTimeout(50000)
+import {Application} from 'spectron'
+import path from 'path'
 
-test('Window Loads Properly', async () => {
-  // Wait for dev server to start
-  const { app, stopServe } = await testWithSpectron(spectron)
-  // const win = app.browserWindow
-  const client = app.client
 
-  // Window was created
-  expect(await client.getWindowCount()).toBe(0)
-  // It is not minimized
-  // expect(await win.isMinimized()).toBe(false)
-  // Window is visible
-  // expect(await win.isVisible()).toBe(true)
-  // Size is correct
-  // const { width, height } = await win.getBounds()
-  // expect(width).toBeGreaterThan(0)
-  // expect(height).toBeGreaterThan(0)
-  // App is loaded properly
-  // expect(
-  //   /Welcome to Your Vue\.js (\+ TypeScript )?App/.test(
-  //     await client.getHTML('#app')
-  //   )
-  // ).toBe(true)
+jest.setTimeout(3 * 60 * 1000)
 
-  await stopServe()
+const app = new Application({
+  path: require('electron'),
+  args: [path.join(__dirname, '..')],
 })
+
+describe('Core', () => {
+  beforeEach(async () => {
+    await app.start()
+  })
+
+  test('Window Loads Properly', async () => {
+    // const win = app.browserWindow
+    const client = app.client
+
+    // Window was created
+    expect(await client.getWindowCount()).toBe(0)
+    // It is not minimized
+    // expect(await win.isMinimized()).toBe(false)
+    // Window is visible
+    // expect(await win.isVisible()).toBe(true)
+    // Size is correct
+    // const { width, height } = await win.getBounds()
+    // expect(width).toBeGreaterThan(0)
+    // expect(height).toBeGreaterThan(0)
+    // App is loaded properly
+    // expect(
+    //   /Welcome to Your Vue\.js (\+ TypeScript )?App/.test(
+    //     await client.getHTML('#app')
+    //   )
+    // ).toBe(true)
+  })
+
+  afterEach(async () => {
+    if (app && app.isRunning()) {
+      await app.stop()
+    }
+  })
+})
+
+
+
+
