8c0b44afd3f7043d6e1b83a092a41642fcef3199
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-06-02 17:26:45 +0200

    hygen BaseInputToggle

---
+++ src/components/BaseInputToggle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex">
+    <base-button></base-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+// import mixins from 'vue-typed-mixins'
+import BaseButton from '@/components/BaseButton.vue'
+
+export default Vue.extend({
+  components: {
+    BaseButton,
+  },
+})
+</script>
---
+++ tests/unit/components/BaseInputToggle.spec.ts
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/vue'
+import Component from "@/components/BaseInputToggle.vue";
+
+describe("components/BaseInputToggle.vue", () => {
+  test.todo("implement tests!")
+  test("", () => {
+    const { getByText } = render(Component)
+  });
+});
