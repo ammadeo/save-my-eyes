e8b45f68a3cda1f183bd48fe9d63029949184930
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-06-04 14:01:02 +0200

    hygen

---
+++ tests/unit/components/BaseButtonToggle.spec.ts
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/vue'
+import Component from "@/components/BaseButtonToggle.vue";
+
+describe("components/BaseButtonToggle.vue", () => {
+  test.todo("implement tests!")
+  test("", () => {
+    const { getByText } = render(Component)
+  });
+});
