62c26cf05d5b9adbb8f0140fa07a0cc3d3d366ee
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-07-09 16:02:06 +0200

    add autostart at login

--- src/background.ts
+++ src/background.ts
@@ -5,12 +5,6 @@ import { isProd, isProdBuild, isDevProdTest } from '@/background/env'
 import { setNewBreak } from '@/background/breaker'
 import { useIpcMain } from '@/background/ipc'
 const isDevelopment = process.env.NODE_ENV !== 'production'
-// import {resolve} from 'path'
-// __dirname = resolve();
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-// let win: BrowserWindow | null
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
@@ -18,18 +12,10 @@ protocol.registerSchemesAsPrivileged([
 ])
 
 app.on('window-all-closed', (e: Event) => e.preventDefault())
+app.setLoginItemSettings({
+  openAtLogin: true
+})
 
-// app.on('activate', () => {
-// On macOS it's common to re-create a window in the app when the
-// dock icon is clicked and there are no other windows open.
-// if (win === null) {
-//   createWindow()
-// }
-// })
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
   if (isDevelopment) {
     try {
@@ -42,7 +28,6 @@ app.on('ready', async () => {
 
   //@ts-ignore
   app.tray = useTray()
-
   setNewBreak({})
 })
 
