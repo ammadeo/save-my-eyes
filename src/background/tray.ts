baa180e5aaca6e5e0a72fd8c50e57959c98501fd
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-07-09 16:54:20 +0200

    add start up balloon

--- src/background/tray.ts
+++ src/background/tray.ts
@@ -2,17 +2,41 @@ import { join } from 'path'
 import { Tray } from 'electron'
 import { isProdBuild } from './env'
 import { createWindowTray } from './windows'
+import { getUserSettingsStore, TypedStore } from './db'
+import { formatDistanceStrict, addSeconds } from 'date-fns'
+import { pl, enGB } from 'date-fns/locale'
+import ElectronStore from 'electron-store'
+const iconPath = isProdBuild
+  ? join('resources', 'app.asar', 'icon.png')
+  : join('public', 'icon.png')
+
+const getNextBreakIn = (store: ElectronStore<TypedStore>) => {
+  const every = store.get('breaks').every
+  const now = new Date();
+  return formatDistanceStrict(now, addSeconds(now, every), {
+    roundingMethod: 'floor',
+    locale: store.get('lang') === 'en' ? enGB : pl,
+  })
+}
+
+const generateBallonContent = () => {
+  const store = getUserSettingsStore()
+  const nextBreakIn = getNextBreakIn(store)
+  return store.get('lang') === 'en' ? `Your eyes are protected. Next break will start in ${nextBreakIn}. Click to find out more` : `Dbamy o Twoje oczy. Następna przerwa rozpocznie się za ${nextBreakIn}. Kliknij, aby dowiedzieć się więcej`
+}
 
 export const useTray = () => {
-  const tray = new Tray(
-    isProdBuild
-      ? join('resources', 'app.asar', 'icon.png')
-      : join('public', 'icon.png')
-  )
+  const tray = new Tray(iconPath)
   tray.setToolTip(
     isProdBuild ? 'Save your eyes' : 'Save your eyes (Development)'
   )
   tray.on('click', createWindowTray)
   tray.on('right-click', createWindowTray)
+  tray.on('balloon-click', createWindowTray)
+  tray.displayBalloon({
+    icon: iconPath,
+    title: 'Save My Eyes',
+    content: generateBallonContent(),
+  })
   return tray
 }
