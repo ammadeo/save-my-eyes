c40c1e2a5632019a12b18078a675fccd1a04ca4e
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-07-17 10:47:09 +0200

    add logs for production test

--- src/background.ts
+++ src/background.ts
@@ -4,19 +4,21 @@ import { useTray } from '@/background/tray'
 import { isProd, isProdBuild, isDevProdTest } from '@/background/env'
 import { setNewBreak } from '@/background/breaker'
 import { useIpcMain } from '@/background/ipc'
+import { info, verbose } from 'electron-log'
+import path from 'path'
 const isDevelopment = process.env.NODE_ENV !== 'production'
-
+const appFolder = path.dirname(process.execPath)
+const appPath = path.resolve(appFolder, 'save-my-eyes.exe')
+let tray: Electron.Tray
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } },
 ])
 
 app.on('window-all-closed', (e: Event) => e.preventDefault())
-app.setLoginItemSettings({
-  openAtLogin: true,
-})
 
 app.on('ready', async () => {
+  verbose('app ready event fired')
   if (isDevelopment) {
     try {
       await installVueDevtools()
@@ -24,11 +26,28 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
-  useIpcMain()
 
-  //@ts-ignore
-  app.tray = useTray()
+  if (
+    !app.getLoginItemSettings({
+      path: appPath,
+    }).openAtLogin
+  ) {
+    app.setLoginItemSettings({
+      openAtLogin: true,
+      path: appPath,
+    })
+    info('app registered to start on login')
+  } else {
+    verbose('not: app registered to start on login')
+  }
+
+  useIpcMain()
+  verbose('app using IPC main module')
+  tray = useTray()
+  verbose('app using Tray module')
   setNewBreak({})
+  verbose('app break set')
+  verbose('app ended init process')
 })
 
 // Exit cleanly on request from parent process in development mode.
--- src/background/tray.ts
+++ src/background/tray.ts
@@ -6,9 +6,11 @@ import { formatDistanceStrict, addSeconds } from 'date-fns'
 import { pl, enGB } from 'date-fns/locale'
 import ElectronStore from 'electron-store'
 import { appIcon } from './paths'
+import { debug } from 'electron-log'
 
 const getNextBreakIn = (store: ElectronStore<TypedStore>) => {
   const every = store.get('breaks').every
+  debug('tray balloon get breaks every', every)
   const now = new Date()
   return formatDistanceStrict(now, addSeconds(now, every), {
     roundingMethod: 'floor',
@@ -16,26 +18,32 @@ const getNextBreakIn = (store: ElectronStore<TypedStore>) => {
   })
 }
 
-const generateBallonContent = () => {
+const generateBalloonContent = () => {
   const store = getUserSettingsStore()
+  debug('tray balloon receive store')
   const nextBreakIn = getNextBreakIn(store)
+  debug('tray balloon next break in', nextBreakIn)
+  debug('tray balloon get lang', store.get('lang'))
   return store.get('lang') === 'en'
-    ? `Your eyes are protected. Next break will start in ${nextBreakIn}. Click to find out more`
-    : `Dbamy o Twoje oczy. Następna przerwa rozpocznie się za ${nextBreakIn}. Kliknij, aby dowiedzieć się więcej`
+    ? `Your eyes are protected. Next break will start in ${nextBreakIn}. Click on icon below to find out more`
+    : `Dbamy o Twoje oczy. Następna przerwa rozpocznie się za ${nextBreakIn}. Kliknij ikonę poniżej, aby dowiedzieć się więcej`
 }
 
 export const useTray = () => {
   const tray = new Tray(appIcon)
+  debug('tray created')
   tray.setToolTip(
     isProdBuild ? 'Save your eyes' : 'Save your eyes (Development)'
   )
+  debug('tray set tooltip')
   tray.on('click', createWindowTray)
   tray.on('right-click', createWindowTray)
-  tray.on('balloon-click', createWindowTray)
+  debug('tray set click handlers')
   tray.displayBalloon({
     icon: appIcon,
     title: 'Save My Eyes',
-    content: generateBallonContent(),
+    content: generateBalloonContent(),
   })
+  debug('tray displayed balloon')
   return tray
 }
