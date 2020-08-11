d9ebc222f89e22655f9a7c29d56869ecf0b445b4
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-08-11 14:01:12 +0200

    link to corect mock file

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
+      '<rootDir>/__mocks__/fileMock.ts',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
